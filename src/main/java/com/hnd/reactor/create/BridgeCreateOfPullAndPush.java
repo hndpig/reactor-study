e4e649f15ea82ed935e970d99ceffcfd8363210a
hndpig <hnd794634951@163.com>
2023-12-07 16:22:50 +0800

    Flux.create 推拉模式（不理解）

--- src/main/java/com/hnd/reactor/create/BridgeCreateOfPullAndPush.java
+++ src/main/java/com/hnd/reactor/create/BridgeCreateOfPullAndPush.java
@@ -2,9 +2,14 @@ package com.hnd.reactor.create;
 
 import com.hnd.reactor.listener.MyEventProcessor;
 import com.hnd.reactor.listener.MyListener;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author hnd
@@ -15,6 +20,7 @@ import java.util.List;
 public class BridgeCreateOfPullAndPush {
     public static void main(String[] args) {
         MyEventProcessor myEventProcessor = new MyEventProcessor();
+
         Flux.create(sink -> {
             myEventProcessor.register(new MyListener<String>() {
                 @Override
@@ -33,11 +39,22 @@ public class BridgeCreateOfPullAndPush {
                 }
             });
             sink.onRequest(n -> {
-                System.out.println("=============="+n);
-                List<String> requests = myEventProcessor.request(Long.bitCount(n));
+                System.out.println("==============" + n);
+                List<String> requests = myEventProcessor.request((int)n);
                 requests.forEach(s -> sink.next(s));
             });
-        }).subscribe(System.out::println);
-        myEventProcessor.publish("h","n","k","a","b","c");
+        }).subscribe(new BaseSubscriber<>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+               request(15);
+            }
+
+            @Override
+            protected void hookOnNext(Object value) {
+                System.out.println(value);
+            }
+        });
+
+        myEventProcessor.publish("h","n","k","a","b","c","d","e","f","g","h");
     }
 }
